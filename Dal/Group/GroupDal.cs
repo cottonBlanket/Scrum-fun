676451dba019cf7debcdee35dca3d10c53bc178d
Danil <donul.mukhametshin.03@gmail.com>
2023-04-15 22:47:47 +0500

    new project

---
+++ Dal/Group/GroupDal.cs
@@ -0,0 +1,11 @@
+﻿using Dal.Base;
+using Dal.User;
+
+namespace Dal.Group;
+
+public class GroupDal: BaseDal<int>
+{
+    public string FullQuote { get; set; }
+    
+    public List<UserDal> Users { get; set; }
+}
